492a61ce6b79b16df738232957b3f8e7b212b1be
justsicklife <justsicklife@gmail.com>
2021-04-03 18:22:15 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import Customer from "./components/Customer";
+import Paper from "@material-ui/core/Paper"
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import { withStyles } from "@material-ui/core/styles"
+
+const styles = {
+  root: {
+    width: "100%",
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+};
 
 const customers = [{
   'id': 1,
@@ -31,12 +48,26 @@ const customers = [{
 
 function App() {
   return (
-    <div className="gray-background">
-      {customers.map(c => {
-        return <Customer key={c.id} {...c} />
-      })}
-    </div>
+    <Paper className={styles.root}>
+      <Table className={styles.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {customers.map(c => {
+            return <Customer key={c.id} {...c} />
+          })}
+        </TableBody>
+      </Table>
+    </Paper>
   );
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,30 +1,18 @@
 import React from 'react';
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+
 
 const Customer = ({ image, id, name, birthday, gender, job }) => {
     return (
-        <div>
-            <CustomerProfile image={image} id={id} name={name} />
-            <CustomerInfo birthday={birthday} gender={gender} job={job} />
-        </div>
-    )
-}
-
-const CustomerProfile = ({ image, name, id }) => {
-    return (
-        <div>
-            <img src={image} alt="profile" />
-            <h2>{name}({id})</h2>
-        </div>
-    )
-}
-
-const CustomerInfo = ({ birthday, gender, job }) => {
-    return (
-        <div>
-            <p>{birthday}</p>
-            <p>{gender}</p>
-            <p>{job}</p>
-        </div>
+        <TableRow>
+            <TableCell>{id}</TableCell>
+            <TableCell><img src={image} alt="profile" /></TableCell>
+            <TableCell>{name}</TableCell>
+            <TableCell>{birthday}</TableCell>
+            <TableCell>{gender}</TableCell>
+            <TableCell>{job}</TableCell>
+        </TableRow>
     )
 }
 
