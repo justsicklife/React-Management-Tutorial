8952230bae4436d8eb81ba144562961ee18bf7b5
justsicklife <justsicklife@gmail.com>
2021-04-03 17:50:53 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import Customer from "./components/Customer";
+
+const customers = [{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '홍길동',
+  'birthday': '961222',
+  'gender': '남자',
+  'job': '대학생'
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '홍길동',
+  'birthday': '960305',
+  'gender': '남자',
+  'job': '프로그래머'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '이순신',
+  'birthday': '301234',
+  'gender': '남자',
+  'job': '사람'
+},
+
+]
 
 function App() {
   return (
     <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>즐거운 괴발</h2>
+      {customers.map(c => {
+        return <Customer key={c.id} {...c} />
+      })}
     </div>
   );
 }
